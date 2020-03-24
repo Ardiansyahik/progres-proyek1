0789507c8e2ab80e8f298c0f43b545363f5c5735
Ardiansyahik <nurardians.56@gmail.com>
2020-03-24 17:50:12 +0700

    progres_proyek1

--- src/project1/Nota.java
+++ src/project1/Nota.java
@@ -193,7 +193,7 @@ public class Nota extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        JOptionPane.showMessageDialog(this, "siap cetak ?");
+        JOptionPane.showMessageDialog(this, "cetak sekarang ?");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
